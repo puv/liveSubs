7b83e2d985489fbb372371bba0fccca93c30a400
puv <dev@puv.bar>
2023-10-13 19:09:12 +0300

    Update button styling

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -53,13 +53,13 @@ function OverlayFooter({ config }) {
 				display: 'flex',
 				gap: '1em',
 			}}>
-				<div className="button closeButton" onClick={() => {
+				<div className="button closeButton btn_success" onClick={() => {
 					$('#overlay').css('display', 'none');
 				}}>
 					<a>{Dictionary['close'][config.lang]}</a>
 				</div>
 
-				<div className="button resetButton" onClick={() => {
+				<div className="button resetButton btn_error" onClick={() => {
 					saveConfig(getNewConfig());
 				}}>
 					<a>{Dictionary['reset'][config.lang]}</a>
@@ -69,21 +69,21 @@ function OverlayFooter({ config }) {
 			<div className="credits">
 				<span dangerouslySetInnerHTML={{ __html: Dictionary['credits'][0][config.lang] }}></span>
 				<span dangerouslySetInnerHTML={{ __html: Dictionary['credits'][1][config.lang] }}></span>
-				<div className='button downloadButton'>
+				<div className='button downloadButton btn_info'>
 					<span dangerouslySetInnerHTML={{ __html: Dictionary['download'][config.lang] }}></span>
 				</div>
 			</div>
 			<div className="language">
-				<div className="button langButton" name='en' onClick={setLanguage}>
+				<div className="button langButton btn_neutral" name='en' onClick={setLanguage}>
 							English
 				</div>
-				<div className="button langButton" name='ja' onClick={setLanguage}>
+				<div className="button langButton btn_neutral" name='ja' onClick={setLanguage}>
 							日本語
 				</div>
-				<div className="button langButton" name='ko' onClick={setLanguage}>
+				<div className="button langButton btn_neutral" name='ko' onClick={setLanguage}>
 							한국인
 				</div>
-				<div className="button langButton" name='zh' onClick={setLanguage}>
+				<div className="button langButton btn_neutral" name='zh' onClick={setLanguage}>
 							中國人
 				</div>
 			</div>
