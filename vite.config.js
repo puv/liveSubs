191ea52fe6896caf5232a63e1a7db4adc9a500cb
puv <dev@puv.bar>
2023-10-19 21:28:22 +0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+	plugins: [react()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.app/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 1420,
+		strictPort: true,
+	},
+	// 3. to make use of `TAURI_DEBUG` and other env variables
+	// https://tauri.app/v1/api/config#buildconfig.beforedevcommand
+	envPrefix: ['VITE_', 'TAURI_'],
 
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        client: "client.html",
-      },
-    }
-  }
+	build: {
+		rollupOptions: {
+			input: {
+				main: 'index.html',
+				client: 'client.html',
+			},
+		}
+	}
 }));
